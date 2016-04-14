d96424b19ce4db0891ee994e3e9a59cac26fbc7c
Chris McKenzie <kristopolous@yahoo.com>
2016-04-13 23:56:16 -0700

    spreading out the naming

--- qr-gen.sh
+++ qr-gen.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-cd src
-for i in *txt; do
-  cat $i | qrencode -ic --output=../dest/${i/txt/png}
-done
+qrgen() {
+  cd src
+  for i in *txt; do
+    cat $i | qrencode -ic --output=../dest/${i/txt/png}
+  done
+  cd ..
+}
+
+label() {
+  cd dest
+  for i in *png; do
+    name=`./namegen.sh`
+    echo $name
+    convert $i -filter box -resize 2048x ../final/$i
+  done
+  cd ..
+}
+
+label
