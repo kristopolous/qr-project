3fcafa8d319fc556ea13a8624e2b48045bf28429
Chris McKenzie <kristopolous@yahoo.com>
2016-04-14 00:23:26 -0700

    only generating that which has yet to be

--- qr-gen.sh
+++ qr-gen.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
 qrgen() {
-  cd src
-  for i in *txt; do
-    cat $i | qrencode -ic --output=../dest/${i/txt/png}
+  echo -e "\nQR code generation"
+  for i in src/*txt; do
+    fname=`basename $i`
+    dest=${fname/txt/png}
+    if [ -e dest/$dest ]; then
+      echo " * $dest exists"
+    else
+      echo " * $dest"
+      cat $i | qrencode -ic --output=dest/$dest
+    fi
   done
-  cd ..
 }
 
 label() {
-  for i in dest/*png; do
+  echo -e "\nLabeling"
+  for  i in dest/*png; do
     fname=`basename $i`
-    name=`./namegen.sh`
-    echo $name
-    convert $i -filter box -resize 2048x -gravity south -font DejaVu-Sans-Mono -pointsize 60 -annotate 0x0 "$name" final/$fname
+    if [ ! -e final/$fname ]; then
+      name=`./namegen.sh`
+      echo " * $name = $fname"
+      convert $i -filter box -resize 2048x -gravity south -font DejaVu-Sans-Mono -pointsize 60 -annotate 0x0 "$name" final/$fname
+    else
+      echo " * $fname already generated"
+    fi
   done
 }
 
+qrgen
 label
+
