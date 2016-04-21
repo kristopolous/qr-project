4913dfb3c8a3a5527386bc7dcf0b0cae7f3dfc3f
Chris McKenzie <kristopolous@yahoo.com>
2016-04-21 20:58:09 +0000

    adding a newline

--- www/address.php
+++ www/address.php
@@ -1,5 +1,5 @@
 <?
 $_POST['date'] = date(DATE_RFC2822);
-file_put_contents("stickers.txt", json_encode($_POST), FILE_APPEND);
+file_put_contents("stickers.txt", json_encode($_POST) . "\n", FILE_APPEND);
 header("Location: /?show=thanks");
 ?>
