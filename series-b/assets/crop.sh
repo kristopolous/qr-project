8f3341400e1640e9ff4860e9d4b4999590cbd8c2
Chris McKenzie <kristopolous@yahoo.com>
2016-04-20 23:45:17 -0700

    adding the crop tools

---
+++ series-b/assets/crop.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+[ -e cropped ] ||mkdir cropped
+
+middle=/tmp/interim.png
+cd images;
+for i in *jpg; do
+  convert $i -rotate 90 $middle
+  ../aspectcrop -a 1020:850 $i  ../cropped/$i
+done
