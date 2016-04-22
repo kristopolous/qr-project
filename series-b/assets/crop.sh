45cb33066780d88a7b559b269268749587df5d88
Chris McKenzie <kristopolous@yahoo.com>
2016-04-22 02:55:47 -0700

    quoting out the auto-crop script

--- series-b/assets/crop.sh
+++ series-b/assets/crop.sh
@@ -5,6 +5,6 @@
 middle=/tmp/interim.png
 cd images;
 for i in *jpg; do
-  convert $i -rotate 90 $middle
-  ../aspectcrop -a 1020:850 $i  ../cropped/$i
+  convert "$i" -rotate 90 $middle
+  ../aspectcrop -a 1020:850 "$i"  ../cropped/"$i"
 done
