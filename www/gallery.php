af37a49136b8655a7a9b82b2b7e173d7c0016876
Chris McKenzie <kristopolous@yahoo.com>
2016-04-21 18:34:00 -0700

    Adding a gallery

---
+++ www/gallery.php
@@ -0,0 +1,113 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Project Poetry LA</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+    <meta content="Project Poetry: Public Art in Public Space" property="og:title" />
+    <meta content="Help spread an appreciation for art and poetry" property="og:description" />
+    <meta content="Help spread an appreciation for art and poetry" name="description" />
+    <meta content="http://www.projectpoetryla.org/images/middle-spread.jpg" property="og:image" />
+    <meta content="http://www.projectpoetryla.org/images/middle-spread.jpg" name="thumbnail" />
+    <meta content="http://www.projectpoetryla.org/" property="og:url" />
+	</head>
+	<body class="landing">
+		<div id="page-wrapper">
+
+			<!-- Banner -->
+				<section id="banner">
+					<h2>Project Poetry LA</h2>
+					<p>Bring public art into public space</p>
+				</section>
+
+			<!-- Main -->
+				<section id="main" class="container">
+
+					<section class="box special">
+						<header class="major">
+							<h2>PDFs for Download</h2>
+              <p>Designed for Avery 5164 compatible sheets (3 1/3" x 4").<br/>
+              Available <a href="https://www.google.com/search?q=avery+5164&tbm=shop">online</a> and at any office supply store.</p>
+						</header>
+						<span class="image featured"><img src="images/middle-spread.jpg" alt="" /></span>
+					</section>
+
+					<section class="box special features">
+						<div class="features-row">
+							<section>
+								<h3>QR Poetry</h3>
+								<p>Classic Poetry in QR codes</p>
+							</section>
+							<section>
+								<h3>Fine Art</h3>
+								<p>Poetry fit into a 4" x 3.33" form-factor in languages as diverse as Los Angeles. Take some time out of your day to smell the flowers and read the sonnet.</p>
+							</section>
+						</div>
+						<div class="features-row">
+							<section>
+								<h3>Text Poetry</h3>
+								<p>Culled from the National Gallery of Art, these small reproductions help remind how close at hand great works of art are if we only choose to enjoy them. On the internet, or at our many local museums.</p>
+							</section>
+							<section>
+								<h3>Free and Reproducible</h3>
+                <p>Follow the instructions to print out your own series or send us your mailing address for a set of free complementary stickers. We also encourage people to place sticker sheets in <a href="https://littlefreelibrary.org/">Little Free Libraries</a>.</p>
+							</section>
+						</div>
+					</section>
+
+
+				</section>
+
+			<!-- CTA -->
+				<section id="cta">
+
+					<h2>Free Stickers by Mail</h2>
+					<p>Receive a free sheet of stickers to put up in your own community. Their are six stickers per sheet. They will be a mix of reproductions from the National Gallery of Art and works by various poets</p>
+
+					<form method=post action=address.php>
+						<div class="row uniform 50%" style='text-align:center'>
+              <div class="8u 12u(mobilep)" style='display:inline-block;float:none'>
+                
+								<input type="text" name="name" id="name" placeholder="Your Name" />
+								<input type="text" name="address[]" id="address_1" placeholder="Address Line 1" />
+								<input type="text" name="address[]" id="address_2" placeholder="Address Line 2" />
+								<input type="text" name="city" id="city" placeholder="City" />
+								<input type="text" name="state" id="state" placeholder="State" />
+								<input type="text" name="zip" id="zip" placeholder="Postal Code" />
+								<input type="email" name="email" id="email" placeholder="Email Address (for confirmation)" />
+								<input type="submit" value="Get Stickers" class="fit" />
+							</div>
+						</div>
+					</form>
+
+				</section>
+
+			<!-- Footer -->
+				<footer id="footer">
+					<ul class="icons">
+						<li><a href="https://twitter.com/projectpoetryla/" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="https://www.facebook.com/ProjectPoetryLA/" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+						<li><a href="https://www.instagram.com/projectpoetryla/" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+					</ul>
+					<ul class="copyright">
+						<li>
+              <a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/80x15.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">project poetry</span> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.</li>
+					</ul>
+				</footer>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollgress.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
