cdee207a5397b861f958e467aa3bc03bce5c5f36
Chris McKenzie <kristopolous@yahoo.com>
2016-05-04 03:20:03 -0700

    adding another image

--- series-b/art/src/get.sh
+++ series-b/art/src/get.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 wget -nc https://upload.wikimedia.org/wikipedia/commons/6/69/Utagawa_Toyoharu_-_A_Winter_Party_-_Google_Art_Project.jpg
 curl "http://wallpaperbeta.com/wallpaper_3840x2160/andrew_wyeth_christina's_world_picture_stuff_ultra_3840x2160_hd-wallpaper-328903.jpg" > christinas_world.jpg
+https://upload.wikimedia.org/wikipedia/commons/7/7e/Henri_Rousseau_010.jpg
