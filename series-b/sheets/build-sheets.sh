ef8348e02edbff0f350ae9eec48ddab6931e6977
Chris McKenzie <kristopolous@yahoo.com>
2016-05-04 01:59:50 -0700

    Adding a sheet builder

--- series-b/sheets/build-sheets.sh
+++ series-b/sheets/build-sheets.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 
+generate() {
+  cd html
+  for i in *html; do
+    pdf=${i/html/pdf}
+    ../wkhtmltopdf \
+      -T 0 -B 0 -R 0 -L 0 \
+      --print-media-type -s Letter \
+      $i ../output/$pdf
+  done
+}
+
 build() {
   what=$1
   page=0
   row=0
   col=0
-  for i in ../$what/output/*.png; do
+
+  cd html
+  for i in ../../$what/output/*.{png,jpg}; do
     if [ $row -eq 0 ]; then 
       if [ $col -eq 0 ]; then
         fname=$what-$page.html
-        cp header.html $fname
+        cp ../header.html $fname
       fi
-      echo "<span id='col$col'>"
+      echo "<span id='col$col'>" >> $fname
     fi
 
-    echo "<img src='$i'>"
+    echo "<img src='$i'>" >> $fname
     (( row ++ ))
     if [ $row -eq 3 ]; then
-      echo "</span>"
+      echo "</span>" >> $fname
       (( col ++ ))
       row=0
     fi
@@ -27,12 +40,12 @@ build() {
       col=0
     fi
 
-    echo "$fname $col $row"
+    #echo "$fname $col $row"
   done
+  cd ..
 }
 
 build prompt
-#/home/chris/proggies/wkhtmltox/bin/wkhtmltopdf -T 0 -B 0 -R 0 -L 0
-#--print-media-type -s Letter
-#/home/chris/code/ghub/qr-project/series-b/sheets/test.html out.pdf
+build art
+generate
 
