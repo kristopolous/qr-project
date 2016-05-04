ffe0837223ba47ae2995b87f489f9a8e249631d5
Chris McKenzie <kristopolous@yahoo.com>
2016-05-04 02:38:13 -0700

    Using the common lib

--- series-b/art/build-art.sh
+++ series-b/art/build-art.sh
@@ -1,11 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+. ../tools/common.sh
 
-[ -e output ] || mkdir output
+_mkdir output
 
 middle=/tmp/interim.png
 cd src
+num=0
 for i in *jpg; do
-  echo $i
+  status $num "$i"
   convert "$i" -rotate 90 $middle
   ../aspectcrop -a 1020:850 "$i"  ../output/"$i"
+  (( num++ ))
 done
