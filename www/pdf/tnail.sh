ea9abbcf0bc3fd35ba14ae863fa15e9fe9495968
Chris McKenzie <kristopolous@yahoo.com>
2016-05-22 21:35:19 -0700

    lq tnail generation

--- www/pdf/tnail.sh
+++ www/pdf/tnail.sh
@@ -2,5 +2,5 @@
 
 cp ../../series-b/sheets/output/*.pdf .
 for i in *.pdf; do
-  convert $i jpg:- | convert jpg:- -resize 420x ${i/pdf/jpg}
+  convert $i jpg:- | convert jpg:- -resize 420x -quality 50 -strip ${i/pdf/jpg}
 done
